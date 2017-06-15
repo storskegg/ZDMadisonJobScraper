19cf3d794f7d8f2ae85680b5eccd0bb096aaf401
Liam Conrad <liam@storskegg.org>
2017-06-14 20:06:36 -0500

    only text if change, send num listings

--- app/app.rb
+++ app/app.rb
@@ -8,14 +8,14 @@ require 'aws-sdk'
 PAGE_URL = "https://www.zendesk.com/jobs/madison/"
 CUR_COUNT = 2
 
-def spam_my_phone()
+def spam_my_phone(num)
     sns = Aws::SNS::Client.new({
         region: 'us-east-1'
     })
 
     response = sns.publish({
         topic_arn: 'arn:aws:sns:us-east-1:076479453187:NotifyMe',
-        message: 'Elizabeth! This is the big one! Submit your resume to ZD, Dummy!'
+        message: "Elizabeth! This is the big one! Submit your resume to ZD, Dummy! There are #{num}"
     })
 
     puts response.message_id
@@ -25,6 +25,6 @@ doc = Nokogiri::HTML(open(PAGE_URL))
 
 jobs = doc.css('.engineeringproduct').xpath("..").css('li > h4')
 
-# if jobs.count != CUR_COUNT
-    spam_my_phone()
-# end
+if jobs.count != CUR_COUNT
+    spam_my_phone(jobs.count)
+end
